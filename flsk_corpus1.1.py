ca8d33d6a46c5bf2eb48a89ca175a493db884db2
akv17 <temakep@list.ru>
2016-11-07 14:55:22 +0300

    flsk_corpus_1.1
    css target styling added

---
+++ flsk_corpus1.1.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+import re
+
+app = Flask(__name__)
+
+ld = {}
+t_in = open('t.txt', 'r').read()
+t = re.split('\.|\?|!', t_in)   
+def srch(x):
+    rawout = [re.sub('\[|\]|\\n?', '', s.lstrip(' ')) for s in t if x.lower() in s.split() or x.capitalize() in s.split()]
+    for s in rawout:
+        flag=False
+        for el in s:
+            if x == el:
+                flag=True
+                el = el.upper()
+        if flag == False:
+            rawout.remove(s)
+    out = [x.split() for x in rawout]
+    return out
+
+#print(srch('болконский'))
+@app.route('/')
+def sform():
+    if 'wd' not in request.args:
+        return render_template('fm2.html')
+    else:
+        curwd = request.args['wd']
+        fout = open('2.txt', 'w')
+        fout.write(curwd)
+        return redirect(url_for('m', word=curwd))
+
+@app.route('/outpt/<word>')
+def m(word):
+    out = srch(word)
+    return render_template('wl2.html', wd1 = word.lower(), wd2 = word.capitalize(), wdl=out)    
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
